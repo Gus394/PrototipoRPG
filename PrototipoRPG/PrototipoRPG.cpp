b6f53db7187e298cb1466cf6ba063f887f078eca
Gus394 <gustavocoelho394@gmail.com>
2022-04-04 17:51:21 -0300

    .

--- PrototipoRPG/PrototipoRPG.cpp
+++ PrototipoRPG/PrototipoRPG.cpp
@@ -1,67 +1,91 @@
-#include <iostream>
+#include <iostream> // Trabalho feito por Gustavo Coelho e João Vitor Custódio na disciplina de Algoritmos e Programação II
+//#include <time.h>
 using namespace std;
 
 struct Arma
 {
-    int min;
-    int max;
+	int min;
+	int max;
 };
 
 struct Jogador
 {
-    int level;
-    int hp;
-    Arma arma;
+	int level;
+	int hp;
+	Arma arma;
 };
 
 struct Inimigo
 {
-    string nome;
-    int hp;
-    Arma arma;
+	string nome;
+	int hp{}; // Chaves para corrigir o aviso "A variável não foi inicializada. Sempre inicialize uma variável de membro (type.6)."
+	Arma arma{};
 };
 
 struct Fase
 {
-    string nome;
-    Inimigo ini[5];
+	string nome;
+	Inimigo ini[5];
 };
 
 template <typename MORREU>
 bool morreu(MORREU x) {
-    if (MORREU.hp <= 0) {
-        return true;
-    }
-    return false;
+	if (x.hp <= 0) {
+		return true;
+	}
+	return false;
 }
 
 template <typename AGRESSOR, typename ALVO>
-ALVO ataque(AGRESSOR x, ALVO y) {
-    return ALVO.hp - rand() % ((AGRESSOR.arma.max + 1) - AGRESSOR.arma.min) + AGRESSOR.arma.min;
+int ataque(AGRESSOR x, ALVO y) {
+	return y.hp - (rand() % (x.arma.max + 1 - x.arma.min) + x.arma.min);
 }
 
-void JogarFase(Jogador x, Fase y) {
-    cout << y.nome << endl;
+void jogarFase(Jogador& jogador, Fase fase) { // Jogador por referência para salvar as mudanças após a fase
+	
+	Inimigo inimigo;
+	cout << fase.nome << "\n\n";
+
+	for (int i = 0; i < 5; i++) {
+		inimigo.arma = fase.ini[i].arma;
+		inimigo.hp = fase.ini[i].hp;
+		cout << "Encontrou o inimigo " << fase.ini[i].nome << "!\n\n";
+
+		while (morreu(inimigo) == false && morreu(jogador) == false) {
+			inimigo.hp = ataque(jogador, inimigo);
+			jogador.hp = ataque(inimigo, jogador);
+			
+			cout << "Jogador atacou: " << fase.ini[i].nome << " ficou com " << inimigo.hp << " de vida.\n";
+			cout << fase.ini[i].nome << " atacou: " << "Jogador ficou com " << jogador.hp << " de vida. ";
+			system("pause");
+		}
+		if (morreu(jogador)) {
+			cout << "\nMorreu...\n";
+			return;
+		}
+		cout << "Inimigo derrotado.\n\n";
+	}
+	cout << " - Fase Completa -\n";
 }
 
 int main()
 {
-    srand(time(NULL));
-    
-    Arma faca = { 3, 4 };
-    Arma lanca = { 1, 8 };
-    Arma machado = { 4, 7 };
-    Arma espada = { 3, 6 };
+	srand(time(NULL));
 
-    Inimigo ini[5];
-    
-    ini[0] = {"goblin", 12, faca};
-    ini[1] = {"ladino", 26, faca};
-    ini[2] = {"goblino", 20, lanca};
-    ini[3] = {"guerreiro", 14, machado};
-    ini[4] = {"executor", 30, machado};
+	Arma faca = { 3, 4 };
+	Arma lanca = { 1, 8 };
+	Arma machado = { 4, 7 };
+	Arma espada = { 3, 6 };
+	
+	Jogador jogador = { 1, 100, espada };
+	
+	Fase floresta;
+	floresta.nome = "Floresta";
+	floresta.ini[0] = { "Goblin", 12, faca };
+	floresta.ini[1] = { "Orc", 18, faca };
+	floresta.ini[2] = { "Guarda", 20, lanca };
+	floresta.ini[3] = { "Esqueleto", 14, machado };
+	floresta.ini[4] = { "Executor", 30, machado };
 
-    Jogador jogador = { 1, 25, espada };
-
-    Fase floresta = { "floresta", ini };
+	jogarFase(jogador, floresta);
 }
